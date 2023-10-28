44ddc1014b673c74a16d1583b2ecbdb224c6f479
MaxSlukovskyi <maksym.slukovskyi@gmail.com>
2023-10-28 23:52:36 +0300

    Initial commit

---
+++ src/main/java/com/slukovskyi/bidorama/BidoramaApplication.java
@@ -0,0 +1,13 @@
+package com.slukovskyi.bidorama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidoramaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BidoramaApplication.class, args);
+    }
+
+}
